f44c2e7cc9f87e37a2fd5bdd532ea46311a64e8f
B533P-23\533 <allo>
2018-11-12 15:28:12 -0500

    no message

--- Save_load/Assets/Scripts/GameController.cs
+++ Save_load/Assets/Scripts/GameController.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
     public static GameController control;
@@ -11,16 +15,31 @@ public class GameController : MonoBehaviour {
 
 
     private void Awake() {
+        DontDestroyOnLoad(gameObject);
         if (control == null) {
-            DontDestroyOnLoad(gameObject);
-            control = this;
-            Attack = 5;
-            Defense = 2;
-            Health = 10;
+         
+            try
+            {
+                LoadGame();
+            }
+            catch
+            {
+                SetDefaultValue();
+            }
+           
+            control = this; 
         } else if (control != this) {
             Destroy(gameObject);
         }
     }
+
+    private void SetDefaultValue()
+    {
+        Attack = 5;
+        Defense = 2;
+        Health = 10;
+    }
+
     private void OnGUI() {
         GUIStyle style = new GUIStyle();
         style.fontSize = 56;
@@ -42,9 +61,42 @@ public class GameController : MonoBehaviour {
     public void IncreaseHealth() {
         Health += 10;
     }
+    public void SaveGame()
+    {
+        FileStream file = File.Open(Application.persistentDataPath + "/gameInfo.dat", FileMode.Create);
+        PlayerData data = new PlayerData();
+        data.health = Health;
+        data.attack = Attack;
+        data.defence = Defense;
+        data.scene = SceneManager.GetActiveScene().buildIndex;
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, data);
+        file.Close();
+
+    }
+    public void LoadGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        if (!File.Exists(Application.persistentDataPath + "/gameInfo.dat"))
+        {
+            throw new Exception("Game file does not exist");
+        }
+        FileStream file = File.Open(Application.persistentDataPath + "/gameInfo.dat", FileMode.Open);
+        PlayerData data = (PlayerData)(bf.Deserialize(file));
+        Health = data.health;
+        Attack = data.attack;
+        Defense = data.defence;
+        SceneManager.LoadScene(data.scene);
+    }
 
 
+}
 
-
-
+[Serializable]
+class PlayerData
+{
+    public int health;
+    public int attack;
+    public int defence;
+    public int scene;
 }
---
+++ Save_load/Assets/Scripts/SaveLoadButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadButton : MonoBehaviour {
+
+	public void save()
+    {
+        GameController.control.SaveGame();
+    }
+
+    public void load()
+    {
+        GameController.control.LoadGame();
+    }
+}
